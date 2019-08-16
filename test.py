3f5fb75e1b76647623b5a98c9bf2c6a1dd1146c9
Artem Shakurov <galavasteg@gmail.com>
2019-08-16 23:46:26 +0300

    readability and renames in test.py

--- test.py
+++ test.py
@@ -8,23 +8,22 @@ initVals = ([], [2], [1, 2, 2, 3, 2, 5],
             [None, None, None], [2, 2, 2, 2],
             [0, 0, 0], [None, 1, 4, 1, 1],
             [2, 1, 3, 2, 2], [2, 1, 2, 3, 2],
-            [0, 1, 4, 3, 2], [2, 2, 0, 3, 2])
+            [0, 1, 4, 3, 2], [2, 2, 0, 3, 2],
+            #TODO: add linked vals
+            )
+valsArgs = (None, 0, 'not listed', 2)
 
-delValsArgs = (None, 0, 'not listed', 2)
 delAllArgs = (False, True)
 DELETE_PARAMS = dict(
     argnames='initVals, delVal, delAll',
-    argvalues=[
-        # TESTS: empty LL or one node in LL
-        *list(product(initVals, delValsArgs, delAllArgs))
-        # TESTS: many Nodes in the LL
-    ])
+    argvalues=list(product(initVals, valsArgs, delAllArgs)))
 
 
 def create_list(vals: list) -> LinkedList:
     list_ = LinkedList()
     [list_.add_in_tail(Node(x)) for x in vals]
     return list_
+
 
 def get_list_vals(list_: LinkedList) -> list:
     values = []
@@ -34,8 +33,9 @@ def get_list_vals(list_: LinkedList) -> list:
         node = node.next
     return values
 
+
 def get_correct_delete_vals(init_vals: list, del_val, del_all: bool):
-    correct_vals = list(init_vals)
+    correct_vals = list(init_vals)  # copy list
     if del_all:
         correct_vals = list(filter(lambda x: x != del_val, correct_vals))
     else:
@@ -76,11 +76,11 @@ def teardown_function(func):
 @pytest.mark.parametrize('vals', ([2, 11, 1], [100], [0], []))
 def test_clean(vals):
     LList = create_list(vals)
-    print(f'init state:', get_list_vals(LList))
+    print('init state:', get_list_vals(LList))
+    print('expected:', [])
     LList.clean()
     result = get_list_vals(LList)
-    print(f'expected:', [])
-    print(f'result:', result)
+    print('result:', result)
     assert result == []
 
 
@@ -91,11 +91,11 @@ class TestDelete(BaseTest):
     def test_delete(self, initVals, delVal, delAll):
         expected = get_correct_delete_vals(initVals, delVal, delAll)
         LList = create_list(initVals)
-        print(f'init state:', get_list_vals(LList))
+        print('init state:', get_list_vals(LList))
         print(f'to del{" ALL" if delAll else ""}:', delVal)
-        print(f'expected:', expected)
+        print('expected:', expected)
         LList.delete(delVal, all=delAll)
         result = get_list_vals(LList)
-        print(f'result:', result)
+        print('result:', result)
         assert result == expected
 
