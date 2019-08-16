6e4b11d564843b938e6cc408564f359f7965ad94
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-16 11:06:24 +0300

    base test class

--- test.py
+++ test.py
@@ -1,2 +1,33 @@
 from linkedList import Node, LinkedList
 
+
+class BaseTest:
+    init_vals = ()
+
+    @classmethod
+    def setup_class(cls):
+        print(f'============= {cls.__name__} STARTED =================')
+        inst = cls()
+        inst.set_init()
+        print('initial state')
+        inst.list.print_all_nodes()
+
+    @classmethod
+    def teardown_class(cls):
+        print(f'============= {cls.__name__} FINISHED ================')
+
+    def setup_method(self, method):
+        self.set_init()
+        print(method.__name__, 'start')
+
+    def teardown_method(self, method):
+        m_name = method.__name__
+        print('output:')
+        self.list.print_all_nodes()
+        print(m_name, 'finish', '\n')
+
+    def set_init(self):
+        self.list = LinkedList()
+        [self.list.add_in_tail(Node(x)) for x in self.init_vals]
+
+
