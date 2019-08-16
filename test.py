d6b5e328980829f1d65c0411598f4794fb4dd08b
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-16 16:19:31 +0300

    r: base test class

--- test.py
+++ test.py
@@ -1,33 +1,48 @@
 from linkedList import Node, LinkedList
 
 
+def get_init(init_vals: list) -> LinkedList:
+    list_ = LinkedList()
+    [list_.add_in_tail(Node(x)) for x in init_vals]
+    return list_
+
+def get_vals(list_: LinkedList) -> list:
+    vs = []
+    node = list_.head
+    while node is not None:
+        vs.append(node.value)
+        node = node.next
+    return vs
+
+def get_ends(list_: LinkedList) -> tuple:
+    return (list_.head, list_.tail)
+
+
+def setup_function(func):
+    print(f'============= {func.__name__} STARTED =================')
+
+def teardown_function(func):
+    print(f'============= {func.__name__} FINISHED ================')
+
+
 class BaseTest:
-    init_vals = ()
+    init_vals = []
 
     @classmethod
     def setup_class(cls):
         print(f'============= {cls.__name__} STARTED =================')
-        inst = cls()
-        inst.set_init()
-        print('initial state')
-        inst.list.print_all_nodes()
+        print(f'initial state: {get_vals(get_init(cls.init_vals))}')
+        print()
 
     @classmethod
     def teardown_class(cls):
         print(f'============= {cls.__name__} FINISHED ================')
 
-    def setup_method(self, method):
-        self.set_init()
+    def setup_method(self, method, *args, **kwargs):
+        self.list = get_init(self.init_vals)
         print(method.__name__, 'start')
 
     def teardown_method(self, method):
-        m_name = method.__name__
-        print('output:')
-        self.list.print_all_nodes()
-        print(m_name, 'finish', '\n')
-
-    def set_init(self):
-        self.list = LinkedList()
-        [self.list.add_in_tail(Node(x)) for x in self.init_vals]
+        print(method.__name__, 'finish', '\n')
 
 
