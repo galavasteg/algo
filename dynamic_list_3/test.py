eaf556b6a1fd9a1d37a1d3882387b4399fce410e
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-21 17:41:11 +0300

    3.2. delete: simple and negative tests

--- dynamic_list_3/test.py
+++ dynamic_list_3/test.py
@@ -49,3 +49,52 @@ DELETE_NEGATIVE_PARAMS = dict(
         (list(range(32)), list(range(17))),
     ])
 
+class TestDelete(BaseTest):
+    @staticmethod
+    def get_correct_delete_res(da: DynArray, del_indices: list):
+        array, count, capacity, fill_percent = da.meta
+        for del_ind in del_indices:
+            try:
+                ind = del_ind if del_ind >= 0 else count - del_ind
+                array.pop(ind)
+            except IndexError:
+                break
+            count = len(array)
+            fill_percent = count / capacity * 100
+            if capacity > 16 and fill_percent < 50:
+                capacity = int(capacity / 1.5)
+                fill_percent = count / capacity * 100
+        return array, count, capacity, fill_percent
+
+    @pytest.mark.parametrize(**DELETE_NEGATIVE_PARAMS)
+    def test_delete_negative(self, init_vals: list, del_indices: list):
+        da = DynArray.create(init_vals)
+        expected = self.get_correct_delete_res(da, del_indices)
+        print('init state:', da.meta)
+        print('del items in indices:', del_indices)
+        print('expected: IndexError("Index is out of bounds"),', expected)
+        with pytest.raises(
+                IndexError, match='Index is out of bounds') as excinfo:
+            for del_ind in del_indices:
+                da.delete(del_ind)
+        e_type, e_val = ((excinfo.typename, str(excinfo.value))
+                         if excinfo else ('', ''))
+        print('result: {et}("{ev}"),'.format(et=e_type, ev=e_val), da.meta)
+        assert (e_type == 'IndexError' and
+                e_val == 'Index is out of bounds' and
+                da.meta == expected)
+
+    @pytest.mark.parametrize(**DELETE_PARAMS)
+    def test_delete(self, init_vals: list, del_indices: list):
+        da = DynArray.create(init_vals)
+        expected = self.get_correct_delete_res(da, del_indices)
+        print('init state:', da.meta)
+        print('del items in indices:', del_indices)
+        print('expected:', expected)
+        for del_ind in del_indices:
+            da.delete(del_ind)
+        result = da.meta
+        print('result:', result)
+        assert result == expected
+
+
