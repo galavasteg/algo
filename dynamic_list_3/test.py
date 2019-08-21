f2014c675333f85ff1d0a9faa8fd8bd76201029f
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-21 17:22:41 +0300

    pytest setts

--- dynamic_list_3/test.py
+++ dynamic_list_3/test.py
@@ -4,3 +4,18 @@ from itertools import product
 from dynamic_list_3.dynArray import DynArray
 
 
+# --------------------------- pytest settings -----------------------
+
+class BaseTest:
+    @classmethod
+    def setup_class(cls):
+        print('============= {} STARTED ================='.format(cls.__name__))
+
+    @classmethod
+    def teardown_class(cls):
+        print('============= {} FINISHED ================'.format(cls.__name__))
+
+    def teardown_method(self, method):
+        print()
+
+
