90823d766dc136e73e65a6a4e445a88fcd05437d
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-28 13:04:40 +0300

    task 6 deque, base class, tests template

---
+++ deque_6/testDeque.py
@@ -0,0 +1,42 @@
+from deque_6.deque import Deque
+
+
+# --------------------------- SIZE ----------------------------------
+
+def test_size():
+    pass
+
+
+# --------------------------- ADD FRONT -----------------------------
+
+def test_add_front():
+    pass
+
+
+# --------------------------- ADD TAIL ------------------------------
+
+def test_add_tail():
+    pass  # TODO: Queue.enqueue
+
+
+# --------------------------- REMOVE FRONT --------------------------
+
+def test_remove_front():
+    pass  # TODO: Queue.dequeue
+
+
+# --------------------------- REMOVE TAIL ---------------------------
+
+def test_remove_tail():
+    pass
+
+
+# --------------------------- MAIN ----------------------------------
+
+if __name__ == '__main__':
+    test_size()
+    test_add_front()
+    test_add_tail()
+    test_remove_front()
+    test_remove_tail()
+
