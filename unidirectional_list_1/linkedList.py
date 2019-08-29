1d3abf4c21395585b83ebe6da393a24b1ecbd87b
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-29 14:12:05 +0300

    r: functions from main.py -> methods of LinkedList

--- unidirectional_list_1/linkedList.py
+++ unidirectional_list_1/linkedList.py
@@ -108,3 +108,45 @@ class LinkedList:
             newNode.next = afterNode.next
             afterNode.next = newNode
 
+    def to_list(self) -> list:
+        values = []
+        node = self.head
+        while node is not None:
+            values.append(node.value)
+            node = node.next
+        return values
+
+    @classmethod
+    def create_list(cls, vals: list):
+        list_ = cls()
+        for x in vals:
+            list_.add_in_tail(Node(x))
+        return list_
+
+    @staticmethod
+    def get_nodes_vals(nodes: list) -> list:
+        nodes_vals = []
+        for n in nodes:
+            n_vals = []
+            node_ = n
+            while node_ is not None:
+                n_vals.append(node_.value)
+                node_ = node_.next
+            nodes_vals.append(n_vals)
+        return nodes_vals
+
+    @classmethod
+    def merge_lists(cls, list1, list2):
+        # + TODO 1.8: Напишите функцию, которая получает на вход
+        #    два связанных списка, состоящие из целых значений,
+        #    и если их длины равны, возвращает список, каждый
+        #    элемент которого равен сумме соответствующих элементов
+        #    входных списков.
+        if list1.len() == list2.len():
+            new_list = cls()
+            n1, n2 = list1.head, list2.head
+            while all((n1, n2)):
+                new_list.add_in_tail(Node(n1.value + n2.value))
+                n1, n2 = n1.next, n2.next
+            return new_list
+
