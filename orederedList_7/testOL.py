53fa4858e4c11c8d87d83647759d18160271eaf8
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-08-30 17:19:02 +0300

    task 7 OrderedList, test setts, TODOs

---
+++ orederedList_7/testOL.py
@@ -0,0 +1,39 @@
+from orederedList_7.orderedList import OrderedList, OrderedStringList
+# TODO: tests from LinkedList2
+
+
+# --------------------------- ADD -----------------------------------
+
+def test_add():
+    pass
+
+
+# --------------------------- COMPARE -------------------------------
+
+def test_compare():
+    pass
+
+
+# --------------------------- FIND ----------------------------------
+
+def test_find():
+    pass
+
+
+# --------------------------- DELETE --------------------------------
+
+def test_delete():
+    pass
+
+
+# --------------------------- MAIN ----------------------------------
+
+if __name__ == '__main__':
+    test_add()
+    test_find()
+    # test_len()
+    # test_get_all()
+    # test_clean()
+    test_compare()
+    test_delete()
+
