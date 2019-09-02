833f1cb7449aa86591f988f515d01dbfca87d1eb
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-09-02 13:51:54 +0300

    r: new tests setts

--- orederedList_7/testOL.py
+++ orederedList_7/testOL.py
@@ -1,17 +1,41 @@
+from itertools import product
+
 from orederedList_7.orderedList import OrderedList, OrderedStringList
-# TODO: tests from LinkedList2
 
 
-# --------------------------- ADD -----------------------------------
+def get_correct_nodes_vals(init_vals: list, val) -> tuple:
+    return (get_node_vals_view(init_vals, init_vals.index(val))
+            if val in init_vals else None)
 
-def test_add():
-    pass
+
+def get_node_vals_view(l_vals: list, ind: int):
+    prev_vals = l_vals[:ind]
+    next_vals = l_vals[ind+1:]
+    return prev_vals, l_vals[ind], next_vals
 
 
-# --------------------------- COMPARE -------------------------------
+VALS_ARGS = (-9, -4, -1, 0, 1, 4, 9)
+INIT_VALS = [[]] + [[v] for v in VALS_ARGS]
+INIT_VALS = tuple(INIT_VALS + [list(VALS_ARGS[:i] + VALS_ARGS[i+1:])
+                               for i in range(len(VALS_ARGS))])
+ASC_ARGS = (True, False)
 
-def test_compare():
-    pass
+
+# --------------------------- pytest settings -----------------------
+
+class BaseTest:
+    @classmethod
+    def setup_class(cls):
+        print('============= {} STARTED ================='.format(cls.__name__))
+
+    @classmethod
+    def teardown_class(cls):
+        print('============= {} FINISHED ================'.format(cls.__name__))
+
+    def teardown_method(self, method):
+        print()
+
+
 
 
 # --------------------------- FIND ----------------------------------
