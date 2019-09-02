598e87486229677e3c3ed64225e678ecdef7051a
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-09-02 17:50:37 +0300

    task 8 HashTable

---
+++ hash_tables_8/testHT.py
@@ -0,0 +1,35 @@
+from hash_tables_8.hashTable import HashTable
+
+
+# --------------------------- SIZE ----------------------------------
+
+def test_hash():
+    pass
+
+
+# --------------------------- ADD FRONT -----------------------------
+
+def test_seek():
+    pass
+
+
+# --------------------------- ADD TAIL ------------------------------
+
+def test_put():
+    pass
+
+
+# --------------------------- REMOVE FRONT --------------------------
+
+def test_find():
+    pass
+
+
+# --------------------------- MAIN ----------------------------------
+
+if __name__ == '__main__':
+    test_hash()
+    test_seek()
+    test_put()
+    test_find()
+
