d248dd01aa9bde7830d2517f5a742d34aa86d1fa
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-09-06 09:59:08 +0300

    task 9 Native Dictionary

---
+++ native_dictionary_9/testND.py
@@ -0,0 +1,10 @@
+from itertools import product
+from math import gcd
+
+import pytest
+
+from native_dictionary_9.nativeDict import NativeDictionary
+
+
+# --------------------------- HASH ----------------------------------
+
---
+++ native_dictionary_9/testND3.3.2.py
@@ -0,0 +1,6 @@
+from itertools import product
+from fractions import gcd
+
+from native_dictionary_9.nativeDict import NativeDictionary
+
+
