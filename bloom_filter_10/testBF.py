290c5f45819c9476516e9ac672069b3ed777ec7d
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-09-09 09:40:50 +0300

    task 10 Bloom Filter

---
+++ bloom_filter_10/testBF.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+import pytest
+
+from bloom_filter_10.bloomFilter import BloomFilter as BF
+
+
---
+++ bloom_filter_10/testBF3.3.2.py
@@ -0,0 +1,5 @@
+from itertools import product
+
+from bloom_filter_10.bloomFilter import BloomFilter as BF
+
+
