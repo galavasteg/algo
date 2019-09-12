11a06dd1d23461a36aef9a52895e62ddae9e8934
GMCS\ashakurov <ashakurov@gmcs.ru>
2019-09-12 19:55:23 +0300

    rm cyrillic

--- bloom_filter_10/bloomFilter.py
+++ bloom_filter_10/bloomFilter.py
@@ -7,7 +7,6 @@ class BloomFilter:
     def __init__(self, f_len):
         self.filter_len = f_len
         self.storage = [0] * self.filter_len
-        # создаём битовый массив длиной f_len ...
 
     @staticmethod
     def hash(s: str, sz: int, mult: int) -> int:
@@ -17,10 +16,6 @@ class BloomFilter:
         return res
 
     def hash1(self, str1):
-        """организуем цикл до длины строки, результат в этом цикле считаем
-        как его версия с предыдущей итерации умноженная на случайное число
-        к которой прибавляется код очередного символа,
-        и берём результат тут же по модулю длины таблицы."""
         mult = 17
         return self.hash(str1, self.filter_len, mult)
 
@@ -29,12 +24,10 @@ class BloomFilter:
         return self.hash(str1, self.filter_len, mult)
 
     def add(self, str1):
-        # добавляем строку str1 в фильтр
         h1, h2 = self.hash1(str1), self.hash2(str1)
         self.storage[h1] = self.storage[h2] = 1
 
     def is_value(self, str1):
-        # проверка, имеется ли строка str1 в
         h1, h2 = self.hash1(str1), self.hash2(str1)
         return all((self.storage[h1], self.storage[h2]))
 
