ce5a75e42444fb7be26470ba719f99fac6b743c1
Artem Shakurov <galavasteg@gmail.com>
2019-09-28 16:36:05 +0300

    f: 11. much more tests

--- set_11/test_set.py
+++ set_11/test_set.py
@@ -5,11 +5,15 @@ import pytest
 from set_11.powerSet import PowerSet
 
 
-VALS_ARGS = ('', 'лещ', 'dca')
-INIT_VALS = (tuple(),) + tuple((v,) for v in VALS_ARGS)
-INIT_VALS = tuple(INIT_VALS + tuple(VALS_ARGS[:i] + VALS_ARGS[i+1:]
-                                    for i in range(len(VALS_ARGS))) +
-                  (VALS_ARGS,))
+VALS_ARGS = ('', 'лещ', 'dca', 'cda', '0', '-115')
+INIT_VALS = ((),) + tuple((v,) for v in VALS_ARGS)
+INIT_VALS = (INIT_VALS +
+             tuple(VALS_ARGS[:i] + VALS_ARGS[i+1:]
+                   for i in range(len(VALS_ARGS))) +
+             (VALS_ARGS,))
+# put a lot of numbers
+INIT_VALS = INIT_VALS + (tuple(map(str, range(20000))),)
+
 PARAMS = dict(
     argnames='vals, val',
     argvalues=tuple(product(INIT_VALS, VALS_ARGS)))
