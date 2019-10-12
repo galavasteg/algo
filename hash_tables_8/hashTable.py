12bb3d8c7c1a0c56eeb4b3ce1812bd6780e8ace4
Artem Shakurov <galavasteg@gmail.com>
2019-10-12 22:28:05 +0300

    8. HashTable doc

--- hash_tables_8/hashTable.py
+++ hash_tables_8/hashTable.py
@@ -1,10 +1,21 @@
 """
-TODO: doc
+Implement the four methods of the HashTable class:
+- hash_fun (value)
+- seek_slot (value) - find the correct slot
+- put (value) - put the value in the slot returned by seek_slot
+- find (value) - check if the value is in the table.
+Write tests for all of these methods.
 """
 
 
 class HashTable:
     def __init__(self, sz, stp):
+        """:param sz: size of table
+        :param stp: count of slots for searching of the next free slot.
+
+        **sz** and **stp** have to be coprime numbers
+        """
+
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
@@ -14,11 +25,12 @@ class HashTable:
         return sum(s.encode()) % sz
 
     def hash_fun(self, value):
-        # TODO: doc
+        """Always return correct slot index.
+        **value** is always string"""
         return self.hash(value, self.size)
 
     def seek_slot(self, value):
-        # TODO: doc
+        """Return free slot index or None"""
         hash_i = i = self.hash_fun(value)
         if self.slots[i] is not None:
             i = (hash_i + self.step) % self.size
@@ -28,14 +40,16 @@ class HashTable:
             return i
 
     def put(self, value):
-        # TODO: doc
+        """Return slot index of put **value** or
+        None if **value** is impossible to put
+        because of collisions"""
         i = self.seek_slot(value)
         if i is not None:
             self.slots[i] = value
         return i  # None or int
 
     def find(self, value):
-        # TODO: doc
+        """Return **value**'s slot index or None"""
         if value in self.slots:
             return self.slots.index(value)
 
