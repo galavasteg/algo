cebfd3ab87e48748426e6fff68533ecf9f0f0379
Artem Shakurov <galavasteg@gmail.com>
2019-10-12 23:03:50 +0300

    9. NativeDictionary doc

--- native_dictionary_9/nativeDict.py
+++ native_dictionary_9/nativeDict.py
@@ -1,5 +1,13 @@
 """
-TODO: EN doc
+Implement next methods of the class:
+ - put(key, value)
+ - is_key(key)
+ - get(key).
+ Write tests:
+ - put the value of an existing key or new,
+ - checking for the presence and absence of keys,
+ - retrieving a value for an existing and missing key.
+ Size of the dictionary not changes.
 """
 
 
@@ -14,17 +22,24 @@ class NativeDictionary:
         return sum(s.encode()) % sz
 
     def hash_fun(self, key):
+        """Always return correct slot index.
+        **key** is always string"""
         return self.hash(key, self.size)
 
     def is_key(self, key):
+        """Check if the **key** is in the dictionary"""
         return key in self.slots
 
     def put(self, key, value):
+        """Put **value** in the slot corresponding
+        to the **key** hash"""
         i = self.hash_fun(key)
         self.slots[i] = key
         self.values[i] = value
 
     def get(self, key):
+        """Return **key**'s value or None if **key**
+        does not exist in the dictionary"""
         if self.is_key(key):
             i = self.hash_fun(key)
             return self.values[i]
