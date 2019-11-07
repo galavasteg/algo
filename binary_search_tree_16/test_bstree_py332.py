b1e31d172c6ed6cc2dde9afb883527f5d0d62298
Artem Shakurov <ashakurov@gmcs.ru>
2019-11-07 10:34:11 +0300

    16. Test: fill an empty tree

--- binary_search_tree_16/test_bstree_py332.py
+++ binary_search_tree_16/test_bstree_py332.py
@@ -38,3 +38,22 @@ assert t.AddKey(59) == -1
 assert t.AddKey(53) == -1
 assert t.AddKey(55) == 11
 
+
+def test_fill_empty_tree():
+    print()
+    t = aBST(3)
+    for elem in (8,4,12,2,6,10,14,1,3,5,7,9,11,13):
+        t.AddKey(elem)
+
+    assert t.FindKeyIndex(15) == -14
+    t.AddKey(15)
+    assert t.FindKeyIndex(15) == 14
+
+    assert t.AddKey(100) == -1
+    assert t.AddKey(10) == 5
+    assert t.AddKey(15) == 14
+
+    assert t.FindKeyIndex(100) is None
+    assert t.FindKeyIndex(8) == 0
+    assert t.FindKeyIndex(1) == 7
+
