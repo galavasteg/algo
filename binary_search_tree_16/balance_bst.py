db999f8b8ebabdccf9c6c1b712c71d5df73d88c6
Artem Shakurov <ashakurov@gmcs.ru>
2019-11-15 15:29:05 +0300

    17. Balanced BST: recursive function prototype

--- binary_search_tree_16/balance_bst.py
+++ binary_search_tree_16/balance_bst.py
@@ -5,10 +5,6 @@ TODO: EN doc
 
 def GenerateBBSTArray(a: list) -> list:
     """TODO: EN doc
-    1. Выбираем центральный элемент исходного
-       отсортированного массива, и делаем его корневым (на
-       первом шаге помещаем его в нулевой элемент итогового
-       массива).
     2. Для левой части по отношению к выбранному элементу
        повторяем этот алгоритм -- индекс корневого элемента
        левой части будет равен индексу левого наследника
@@ -19,6 +15,15 @@ def GenerateBBSTArray(a: list) -> list:
        корня из пункта 1.
     """
     assert a
+    sorted_a = tuple(sorted(a))
+    res = [None] * len(sorted_a)
 
-    return []
+    def balance_tree_vals(tree_keys: tuple, root_i: int):
+        if tree_keys:
+            center_i = len(tree_keys) // 2
+            res[root_i] = tree_keys[center_i]
+
+    balance_tree_vals(sorted_a, 0)
+
+    return res
 
