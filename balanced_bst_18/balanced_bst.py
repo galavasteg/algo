c8f8473611de2089d13425fe8f01685f81080f62
Artem Shakurov <galavasteg@gmail.com>
2019-11-23 15:31:35 +0300

    #18 BalancedBST: GenerateTree method, tests

--- balanced_bst_18/balanced_bst.py
+++ balanced_bst_18/balanced_bst.py
@@ -48,11 +48,38 @@ class BalancedBST:
     def Count(self) -> int:
         return len(self._get_all_nodes())
 
+    @classmethod
+    def _get_root_index(cls, tree_keys: tuple) -> int:
+        root_i = len(tree_keys) // 2
+        left_i = root_i - 1
+        while (0 <= left_i
+               and tree_keys[left_i] == tree_keys[root_i]):
+            root_i = left_i
+            left_i -= 1
+
+        return root_i
+
+    @classmethod
+    def _keys2tree(cls, tree_keys: tuple,
+                  parent: (BSTNode, None)) -> (BSTNode, None):
+        if tree_keys:
+            root_i = cls._get_root_index(tree_keys)
+
+            child = BSTNode(tree_keys[root_i], parent)
+
+            child.LeftChild = cls._keys2tree(
+                    tree_keys[:root_i], child)
+            child.RightChild = cls._keys2tree(
+                    tree_keys[root_i + 1:], child)
+
+            return child
+
     def GenerateTree(self, a: list):
         """TODO: EN doc
         """
         sorted_keys = tuple(sorted(a))
-        
+        self.Root = self._keys2tree(sorted_keys, parent=None)
+
     def IsBalanced(self, root_node: BSTNode):
         """
         - правое поддерево каждого узла сбалансировано;
