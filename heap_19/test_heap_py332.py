d357800d67ed2425c763af92efc51e57f3ee6749
Artem Shakurov <ashakurov@gmcs.ru>
2019-11-29 15:25:45 +0300

    #19 GetMax, tests wrap as function

--- heap_19/test_heap_py332.py
+++ heap_19/test_heap_py332.py
@@ -9,62 +9,63 @@ Heap.__repr__ = lambda x: 'BST r:%s c:%s' % (
 
 # --------------------------- TESTS ---------------------------------
 
-h = Heap()
-h._array_init(depth=3)
-test_a = [
-        11,
-        9, 4,
-        7, 8, 3, 1,
-        2, 5, 6, None, None, None, None, None,
-    ]
-assert len(test_a) == len(h.HeapArray)
-h.HeapArray = test_a
-assert h.GetMax() == 11
-assert h.HeapArray[0] == 9
-assert 11 not in h.HeapArray
-assert h.GetMax() == 9
-assert h.HeapArray[0] == 8
-assert 9 not in h.HeapArray
-assert h.HeapArray == [
-        8,
-        7, 4,
-        5, 6, 3, 1,
-        2, None, None, None, None, None, None, None,
-    ]
+def test_get_max():
+    h = Heap()
+    h._array_init(depth=3)
+    test_a = [
+            11,
+            9, 4,
+            7, 8, 3, 1,
+            2, 5, 6, None, None, None, None, None,
+        ]
+    assert len(test_a) == len(h.HeapArray)
+    h.HeapArray = test_a
+    assert h.GetMax() == 11
+    assert h.HeapArray[0] == 9
+    assert 11 not in h.HeapArray
+    assert h.GetMax() == 9
+    assert h.HeapArray[0] == 8
+    assert 9 not in h.HeapArray
+    assert h.HeapArray == [
+            8,
+            7, 4,
+            5, 6, 3, 1,
+            2, None, None, None, None, None, None, None,
+        ]
 
-test_a = [
-        11,
-        4, 9,
-        3, 1, 7, 8,
-        None, None, None, None, 2, 5, 6, None,
-    ]
-assert len(test_a) == len(h.HeapArray)
-h.HeapArray = test_a
-assert h.GetMax() == 11
-assert h.HeapArray[0] == 9
-assert 11 not in h.HeapArray
-assert h.GetMax() == 9
-assert h.HeapArray[0] == 8
-assert 9 not in h.HeapArray
-assert h.HeapArray == [
-        8,
-        4, 7,
-        3, 1, 5, 6,
-        None, None, None, None, 2, None, None, None,
-    ]
+    test_a = [
+            11,
+            4, 9,
+            3, 1, 7, 8,
+            None, None, None, None, 2, 5, 6, None,
+        ]
+    assert len(test_a) == len(h.HeapArray)
+    h.HeapArray = test_a
+    assert h.GetMax() == 11
+    assert h.HeapArray[0] == 9
+    assert 11 not in h.HeapArray
+    assert h.GetMax() == 9
+    assert h.HeapArray[0] == 8
+    assert 9 not in h.HeapArray
+    assert h.HeapArray == [
+            8,
+            4, 7,
+            3, 1, 5, 6,
+            None, None, None, None, 2, None, None, None,
+        ]
 
-test_a = [
-        11,
-        None, None,
-        None, None, None, None,
-        None, None, None, None, None, None, None, None,
-    ]
-assert len(test_a) == len(h.HeapArray)
-h.HeapArray = test_a
-assert h.GetMax() == 11
-assert h.HeapArray[0] is None
-assert h.HeapArray == [None] * len(test_a)
+    test_a = [
+            11,
+            None, None,
+            None, None, None, None,
+            None, None, None, None, None, None, None, None,
+        ]
+    assert len(test_a) == len(h.HeapArray)
+    h.HeapArray = test_a
+    assert h.GetMax() == 11
+    assert h.HeapArray[0] is None
+    assert h.HeapArray == [None] * len(test_a)
 
-assert h.GetMax() == -1
+    assert h.GetMax() == -1
 
 
